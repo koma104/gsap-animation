69c786970f595d3d7d47981d80aea41b005708ec
ToshihitoKomatsu <komatsu@dsnf.co.jp>
2025-05-26 15:26:31 +0900

    chore: devサーバー起動時にanime01.htmlを自動オープンするよう設定

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   css: {
     devSourcemap: true, // 開発時のCSSソースマップ有効化
-  }
-}
+  },
+  server: {
+    open: '/anime01.html', // ここで開きたいHTMLファイルを指定
+  },
+});
